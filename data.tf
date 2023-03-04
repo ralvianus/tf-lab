dbc8907208755498f9d6a810c179562c84ffdce4
Robert Alvianus <robert@alvianus.net>
2023-03-05 02:14:19 +0800

    update image

--- data.tf
+++ data.tf
@@ -7,18 +7,18 @@ data "aws_key_pair" "key" {
   include_public_key = true
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["aws-marketplace"]
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#   owners      = ["aws-marketplace"]
 
-  filter {
-    name   = "name"
-    values = ["2022-06-01T11-22-27.662Z-cbdef1bf-8e5a-4a36-acdc-ca2b59bd3702"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["2022-06-01T11-22-27.662Z-cbdef1bf-8e5a-4a36-acdc-ca2b59bd3702"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+# }
 
