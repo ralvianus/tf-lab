7ad7b79fd85795b50ad08d3642fe37d652782731
Robert Alvianus <robert@alvianus.net>
2023-03-05 02:41:02 +0800

    update module

---
+++ modules/aws-vpc/instance.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "web_server" {
+  count = 1
+  ami   = "ami-09f238ada65bd0563"
+  instance_type = "t3.micro"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.public[count.index].id
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
+
+  # the public SSH key
+  key_name = var.key_pair_name
+
+  tags = {
+    Name = "${var.name_prefix}-web-${count.index}"
+  }
+}
---
+++ modules/aws-vpc/networks.tf
@@ -0,0 +1,33 @@
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.default.id
+  tags = {
+    Name = "${var.name_prefix}-igw"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidr_blocks)
+
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = var.public_subnet_cidr_blocks[count.index]
+  availability_zone       = data.aws_availability_zones.azs.names[count.index]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.name_prefix}-subnet-${count.index}"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.default.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.default.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidr_blocks)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
